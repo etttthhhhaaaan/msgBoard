326998efa10c4a547f5fe05b88d795f97ef89493
Ethan <etttthhhhaaaan@gmail.com>
2023-10-12 23:41:01 -0400

    updated color schema

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-emerald-300">
+      <body className="bg-cyan-200">
         <div className="max-w-5xl mx-auto p-4">
           
           <NavbarTwo />
--- components/NavbarTwo.jsx
+++ components/NavbarTwo.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavbarTwo() {
   return (
-    <nav className="flex justify-between items-center bg-emerald-700 px-8 py-3">
+    <nav className="rounded-3xl flex justify-between items-center bg-cyan-400 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
         46 E Northwood house chores!
       </Link>
